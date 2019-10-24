5a01ebb8ed03ff304e83b5b0e949464399b2d1d7
Daniel Finnerman <danielfi@metropolia.fi>
2019-10-24 13:44:03 +0300

    Initial commit

---
+++ js/main-b.js
@@ -0,0 +1,11 @@
+const main = document.querySelector("main");
+main.innerHTML += `<article>
+                <header> 
+                    <h2>Scripted article header</h2>
+             </header>
+                <figure>
+                    <img src=\"http://placekitten.com/320/160\" alt=\"title\">
+                    <figcaption>Scripted caption</figcaption>
+                </figure>
+                <p>Scripted text scripted text asdasdsadsadsads</p>
+         </article>`
